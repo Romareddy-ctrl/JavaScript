e51d3ecd2cbb2772d478d77fcfd1d26deff0fe3e
Roma <romareddy6565@gmail.com>
2025-11-27 16:18:20 +0530

    Add files via upload

---
+++ conversions.js
@@ -0,0 +1,16 @@
+//TYPE COERCION
+
+
+//TYPE CASTING
+let a=String(1);
+console.log(a);
+console.log(typeof a);
+
+
+let b=Number("abc");
+console.log(b);
+console.log(typeof b);
+
+let c=Boolean(1);
+console.log(c);
+console.log(typeof c);

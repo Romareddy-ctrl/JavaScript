76e10f762956be34c5e72378586224bca629e277
Roma <romareddy6565@gmail.com>
2025-11-28 13:14:25 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,25 @@
+let a=5;
+let b=3;
+console.log("a + b= ",a+b)
+
+
+
+let c=5;
+console.log("a += 2= ",c+=2)
+
+
+let x=8;
+let y=10;
+console.log("x == y = ",x==y);
+
+let e=2;
+let f=6;
+let cond1 = e>f;
+let cond2 = e===2;
+console.log(cond1 && cond2);
+
+
+let w=3;
+let v=(w--)+(++w)-(--w)+(w++);
+console.log(w);
+console.log(v);
